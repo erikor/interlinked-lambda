519f2e2cd67d3abbd5b4092ed327435b58cdb9f8
EC2 Default User <ec2-user@ip-172-31-30-26.us-east-2.compute.internal>
2018-10-27 15:43:55 +0000

    upload endpoint

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,6 +1,13 @@
 import unittest
 import index
 import upload
+import sys
+try:
+    import boto3
+except ImportError:
+    sys.path.append('lib')
+    import boto3
+import json
 
 class TestHandlerCase(unittest.TestCase):
 
@@ -14,10 +21,20 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_upload(self):
         print("testing upload.")
-        result = upload.handler(None, None)
+        result = upload.handler({"body" : '{"key": "1", "data": "[1,2,3,4,5]"}'}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
+
+    def test_s3(self):
+        data = [19, "A", 43, "B"]
+        s3 = boto3.resource('s3')
+        obj = s3.Object('interlinked','test/hello.tab')
+        obj.put(Body='\n'.join(str(e) for e in data))
+        obj = s3.Object('interlinked','test/hello.tab')
+        data = obj.get()['Body'].read().decode('utf-8') 
+        print("testing s3.")
+        print data
 
 if __name__ == '__main__':
     unittest.main()
--- upload.py
+++ upload.py
@@ -8,7 +8,18 @@ import json
 import datetime
 
 def handler(event, context):
+    #data = [19, "A", 43, "B"]
+    #s3 = boto3.resource('s3')
+    #obj = s3.Object('interlinked','test/hello.tab')
+    #obj.put(Body='\n'.join(str(e) for e in data))
+    #obj = s3.Object('interlinked','test/hello.tab')
+    #data = obj.get()['Body'].read().decode('utf-8') 
+
+    #obj.put(Body='\n'.join(str(e) for e in data))
+    payload = json.loads(event['body'])
+    key = payload['key']
+    data = payload['data']
     return {'statusCode': 200,
-            'body': json.dumps(event),
+            'body': "\n".join(str(s) for s in data),
             'headers': {'Content-Type': 'application/json'}}
             
